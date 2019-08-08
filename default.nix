ead439dfd759f5475809b3c9332511fc25531af8
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-08 14:26:01 +0300

    Use php56Packages from overlay

--- default.nix
+++ default.nix
@@ -109,38 +109,6 @@ sh = dash.overrideAttrs (_: rec {
   postInstall = ''(cd "$out/include" && ln -s ImageMagick* ImageMagick)'';
  };
 
- buildPhp56Package = args: buildPhpPackage ({ php = php56; } // args);
-
-
-php56Packages = {
-  timezonedb = buildPhp56Package {
-    name = "timezonedb";
-    version = "2019.1";
-    sha256 = "0rrxfs5izdmimww1w9khzs9vcmgi1l90wni9ypqdyk773cxsn725";
-  };
-
-  dbase = buildPhp56Package {
-      name = "dbase";
-      version = "5.1.0";
-      sha256 = "15vs527kkdfp119gbhgahzdcww9ds093bi9ya1ps1r7gn87s9mi0";
-  };
-
-  intl = buildPhp56Package {
-      name = "intl";
-      version = "3.0.0";
-      sha256 = "11sz4mx56pc1k7llgbbpz2i6ls73zcxxdwa1d0jl20ybixqxmgc8";
-      inputs = [ icu58 ];
-  };
-
-  imagick = buildPhp56Package {
-      name = "imagick";
-      version = "3.1.2";
-      sha256 = "528769ac304a0bbe9a248811325042188c9d16e06de16f111fee317c85a36c93";
-      inputs = [ pkgconfig imagemagick68 pcre ];
-      configureFlags = [ "--with-imagick=${imagemagick68}" ];
-  };
-};
-
   rootfs = mkRootfs {
       name = "apache2-php56-rootfs";
       src = ./rootfs;
